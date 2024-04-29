bf3301df0fac3044de235890e2da3bc5aa4f632c
vkhalash <159003530+vkhalash@users.noreply.github.com>
2024-04-29 20:57:33 +0300

    First commit

--- OOPLab2Tpl/OOPLab2Tpl.cpp
+++ OOPLab2Tpl/OOPLab2Tpl.cpp
@@ -1,38 +1,81 @@
-﻿// OOPLab2Tpl.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
 #include <iostream>
+
 using namespace std;
-#include "Tasks.h"
-#include "Examples.h"
 
+void MenuTask()
+{
+    cout << "    Menu Task\n";
+    cout << "    1.  Calculation of expressions using bitwise operations\n";
+    cout << "    2.  Data encryption using bitwise operations\n";
+    cout << "    3.  Exit\n";
+}
 
-/// @brief 
-/// @return 
+/*
+Обчислення виразів з використанням побітових операцій.
+Задано цілі числа a, b, c та d. Обчислити вираз без використання операцій множення та ділення
+(замінивши їх операціями зсувів).
+Вираз: 2049 * b + ((d * 15 + 12 * a) / 2048) - c * 100 + d * 104
+*/
+void Task1()
+{
+    int a, b, c, d, x, y, z;
+
+    cout << "An example of calculating an expression using only bitwise operations" << endl;
+    cout << "Expression: x = 2049 * b + ((d * 15 + 12 * a) / 2048) - c * 100 + d * 104" << endl;
+    cout << "Input a, b, c, d ";
+
+    cin >> a >> b >> c >> d;
+
+    // Check input state
+    auto st = cin.rdstate();
+    if (st != cin.goodbit)
+    {
+        cin.clear();
+        cout << "Error: invalid input.\n";
+        cin.get();
+        cin.get();
+        return;
+    }
+
+    // Calcucalte expression
+    x = 2049 * b + ((d * 15 + 12 * a) / 2048) - c * 100 + d * 104;                                                                                // Check with simple multiplication and division
+    y = (2048 * b + b) + (((d * 16 - d) + (8 * a) + (4 * a)) / 2048) - ((128 * c) - (32 * c) + (4 * c)) + ((128 * d) - (16 * d) - (8 * d));       // Checking with multiplications and divisions by 2^n
+    z = ((b << 11) + b) + ((((d << 4) - d) + ((a << 3) + (a << 2))) >> 11) - ((c << 6) + (c << 5) + (c << 2)) + ((d << 6) + (d << 5) + (d << 3)); // Checking with bitwise operations
+
+    cout << "x " << x << "\ny " << y << "\nz " << z << "\na " << a << "\nb " << b << "\nc " << c << "\nd " << d << endl;
+}
+
 int main()
 {
-    cout << "OOP. Template for laboratory work #2.\n";
+    MenuTask();
 
-    char ch = '5';
-    do {
-        system("cls");
-        MenuTask();
-        ch = cin.get();
-        
-        cin.get();
+    int choice;
 
-        switch (ch) {
-        case '1': task1();   break;
-        case '2': task1();   break;
-        case '3': task1();   break;
-        case '4': task1();   break;
-		case '5':  Examples(); break;
-		case '6': return 0;
-	    }
-        cout << " Press any key and enter\n";
-        ch = cin.get();
-    } while (ch != '6');
+    do
+    {
+        cout << "Choice ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1: // Task 1
+        {
+            Task1();
+            choice = 3;
+            break;
+        }
+        case 2: // Task 2
+        {
+            choice = 3;
+            break;
+        }
+        case 3: // Exit
+            break;
+        default:
+            cout << "Invalid choice\n";
+            break;
+        }
+    } while (choice != 3);
 
     return 0;
 }
-
